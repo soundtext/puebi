a18ce4b8c066d091c8d8bd720a6180d9bb68326c
aproinov <98790856+aproinov@users.noreply.github.com>
2023-08-15 05:48:42 +0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "PUEBI Project"
+copyright = "2023, PUEBI core team"
+author = "PUEBI core team"
 
 
 # -- General configuration ---------------------------------------------------
